e10f47450d5a353c385706a052b6c686d62d1c18
ostrev <npostrev@gmail.com>
2023-04-25 19:55:26 +0300

    delete

--- Advanced/lists_as_stacks_and_queues/reverse_numbers.py
+++
@@ -1,3 +0,0 @@
-data = input().split()
-while data:
-    print(data.pop(), end=' ')
--- Advanced/lists_as_stacks_and_queues/reverse_strings.py
+++
@@ -1,4 +0,0 @@
-string_as_stack= list(input())
-
-while string_as_stack:
-    print(''.join(string_as_stack.pop()), end='')
