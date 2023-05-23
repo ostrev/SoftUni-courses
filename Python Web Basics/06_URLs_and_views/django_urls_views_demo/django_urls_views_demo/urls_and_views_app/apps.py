ed0fe0b0e632c6eb2309faafd1eb2f632d92c99f
ostrev <npostrev@gmail.com>
2023-05-23 14:37:32 +0300

    create a new project, add app, urls and views

---
+++ Python Web Basics/06_URLs_and_views/django_urls_views_demo/django_urls_views_demo/urls_and_views_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UrlsAndViewsDemoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_urls_views_demo.urls_and_views_app'
